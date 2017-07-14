966591450df34eb64dbecdfca130b825d7f0949d
Ivan Ivanov <ivan@ivanovs.info>
2017-07-15 06:19:06 +1200

    fefe program works

--- mips/processor/singlecycle/tests/processor/fefe/test_fefe.v
+++ mips/processor/singlecycle/tests/processor/fefe/test_fefe.v
@@ -12,7 +12,13 @@ module test_fefe;  // not covfefe
         $readmemb("tests/processor/fefe/fefe_imem.dat", CPU.IFU.imemory.storage.bytes);
         $readmemh("tests/processor/fefe/fefe_dmem.dat", CPU.dmemory.bytes);
 
-        repeat(19) @(posedge CPU.clk);
+        repeat(9) @(posedge CPU.clk);
+
+        `assertEq(CPU.registers.registers[`REG_S0], 32'hFEFEFEFE)
+        `assertEq(CPU.registers.registers[`REG_S1], 32'd64)
+        `assertEq(CPU.registers.registers[`REG_T0], 32'h0)
+
+        repeat(32) @(posedge CPU.clk);
 
         `assertEq(CPU.dmemory.bytes[0], 8'hFE)
         `assertEq(CPU.dmemory.bytes[1], 8'hFE)
